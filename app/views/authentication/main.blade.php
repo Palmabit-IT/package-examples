3da65c2b5dc0c1797aebd972b77bedcd8b8d36a8
Jacopo <beschi.jacopo@gmail.com>
2014-10-14 18:04:50 +0200

    authentication base

--- app/views/authentication/main.blade.php
+++ app/views/authentication/main.blade.php
@@ -12,18 +12,24 @@
         <li><a href="#permission">User and permissions crud</a></li>
         <li><a href="#login">Login/Logout</a></li>
     </ul>
+    In order to try it run the following commands:
+     <ol>
+        <code><li>composer update</code></li>
+        <code><li>php artisan authentication:install</code></li>
+     </ol>
     </p>
 
     <h1 id="menu">Customizable menu</h1>
     <p>
-
+        You can add and remove field in the menu editing the file in "app/config/packages/palmabit/authentication/menu.php". Chekc the comment in the file for more info
     </p>
 
-    <h1 id="permission">User and permissions crud</h1>
-    <p></p>
+    <h1 id="permission">User, groups and permissions crud</h1>
+    <p>
+       This package allow you to handle users via admin panel, to try that <a href="{{URL::to('admin/users/list',false)}}">follow this link</a> (user:admin@admin.com, password:admin)
+    </p>
 
     <h1 id="login">Login/Logout</h1>
-    <p></p>
 @stop
 @section('scripts')
     @parent
