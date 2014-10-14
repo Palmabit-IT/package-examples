198f82e3fdb2640d90f128278201341221cd8319
Jacopo <beschi.jacopo@gmail.com>
2014-10-14 17:29:52 +0200

    fix

--- app/views/packages/palmabit/authentication/layouts/base-1cols.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('authentication::layouts.base')
-
-@section('container')
-<div class="row">
-    <div class="col-mg-12">
-        @yield('content')
-    </div>
-</div>
-@stop
--- app/views/packages/palmabit/multilanguage/select-language.blade.php
+++
@@ -1,1 +0,0 @@
-{{Form::select('language',$urls,URL::to(\Request::decodedPath() , true , $current_language), ['id' => 'change-language'])}}
